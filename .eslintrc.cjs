d2e666f894cda91e4df01b2a2958eeaa9913f35e
Zhekai <limzhekai1999@live.com>
2024-08-01 23:57:48 +0800

    Add packages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,10 +5,11 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    "plugin:css-modules/recommended"
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'css-modules'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
