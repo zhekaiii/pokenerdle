953f7cad5f1f967a271d3d29649df7f515b971ee
Zhekai <limzhekai1999@live.com>
2024-12-20 13:48:15 +0800

    Migrate yarn to npm

--- shared/src/index.d.ts
+++ shared/src/index.d.ts
@@ -1,4 +1,4 @@
-import { pokemon_v2_ability, pokemon_v2_pokemon } from "./generated/prisma";
+import { pokemon_v2_ability, pokemon_v2_pokemon } from "@prisma/client";
 
 export type PokemonWithAbilities = pokemon_v2_pokemon & {
   abilities: pokemon_v2_ability[];
