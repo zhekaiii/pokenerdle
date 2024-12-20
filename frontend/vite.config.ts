0025b33f573f4f6bb4e3ccd67054440aa8dc3a6f
Zhekai <limzhekai1999@live.com>
2024-12-20 18:51:36 +0800

    Fix deprecation warnings for sass

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,4 +14,11 @@ export default defineConfig({
       typescript: true,
     }),
   ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler",
+      },
+    },
+  },
 });
