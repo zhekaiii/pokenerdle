35205440672570e8bf4f0a12f0106ca14a7168c5
Zhekai <limzhekai1999@live.com>
2024-12-20 13:48:37 +0800

    Uninstall redis from backend

--- backend/src/data/cache.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "redis";
-
-export const redisClient = createClient();
-redisClient.on("error", (error) => {
-  console.error(error);
-});
-
-await redisClient.connect();
