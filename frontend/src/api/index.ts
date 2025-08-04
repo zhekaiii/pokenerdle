4651c30108d9909cbb8e6595c2ab4c2705c6fb85
Zhekai <limzhekai1999@live.com>
2025-08-04 22:14:55 +0800

    refactor(build): simplify build scripts and fix api url handling in prod

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -3,7 +3,9 @@ import battles from "./battles";
 import data from "./data/data";
 import pathfinder from "./pathfinder/pathfinder";
 
-axios.defaults.baseURL = `${import.meta.env.VITE_BACKEND_URL}/api`;
+axios.defaults.baseURL = import.meta.env.PROD
+  ? "/api"
+  : `${import.meta.env.VITE_BACKEND_URL}/api`;
 
 const api = {
   data,
