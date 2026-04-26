4c409b2a73bfdcfa58aa57b0b73274bc425f1d89
Zhekai <limzhekai1999@live.com>
2026-04-26 11:45:32 +0800

    feat(database): update preprocess.sql to add new sprite URLs for Pokémon forms 10278 to 10325

--- backend/prisma-sqlite/preprocess.sql
+++ backend/prisma-sqlite/preprocess.sql
@@ -10,6 +10,18 @@ JOIN
     pokemon_v2_pokemon p ON pf.pokemon_id = p.id
 WHERE
     p.name LIKE '%-gmax';
+
+UPDATE pokemon_v2_pokemonsprites
+SET sprites = json_set(
+    sprites,
+    '$.other.home.front_default',
+    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/' || pokemon_id || '.png',
+    '$.other.home.front_shiny',
+    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/shiny/' || pokemon_id || '.png'
+)
+WHERE
+    pokemon_id BETWEEN 10278 AND 10325
+    AND json_extract(sprites, '$.other.home.front_default') IS NULL;
 INSERT INTO pokemon_v2_pokemonformname (pokemon_name, pokemon_form_id, language_id, name)
 SELECT
     '',
